2e14bf645ec320fc0caf6d6afa01698bac28b18f
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-12-01 21:32:55 -0500

    feat - Ignacio Castillo: implementación de la documentación en swagger dentro del api gateway

--- src/user-management/dto/change-status.dto.ts
+++ src/user-management/dto/change-status.dto.ts
@@ -1,7 +1,13 @@
 import { Type } from 'class-transformer';
 import { IsInt, IsPositive } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ChangeStatusDto {
+  @ApiProperty({
+    description: 'ID del nuevo estado a asignar al usuario (1: activo, 2: inactivo, 3: suspendido)',
+    example: 1,
+    type: Number
+  })
   @IsInt()
   @IsPositive()
   @Type(() => Number)
