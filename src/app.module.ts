d9b2ea01faded725e87d5e89da87be5cc5110463
Anderson Fabian Garcia Nieto <anderson.fgn13@gmail.com>
2025-12-07 13:51:54 -0500

    feat(pruebas): Se estan realizando las resepectivas preubas de conectividad

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserManagementModule } from './user-management/user-management.module';
 import { NotificacionesModule } from './notificaciones/notificaciones.module';
+import { IaModule } from './ia/ia.module';
 
 @Module({
-  imports: [AuthModule, UserManagementModule, NotificacionesModule],
+  imports: [
+    AuthModule,
+    UserManagementModule,
+    NotificacionesModule,
+    IaModule,
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
--- src/ia/ia.controller.ts
+++ src/ia/ia.controller.ts
@@ -27,6 +27,7 @@ export class IaController {
 
   @Post('chat')
   async chat(@Body() body: any) {
+      console.log('Chat endpoint hit with body:', body); // Agrega este log
     return this.iaService.chat(body);
   }
 
