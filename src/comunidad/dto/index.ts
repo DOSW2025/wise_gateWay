cc1acd6c847c2ac31ef2bfa23a62ddf88713cbd6
ignacio14.castillor@gmail.com <ignacio.castillo-r@mail.escuelaing.edu.co>
2025-12-13 22:46:01 -0500

    feat - Ignacio Castillo: Conexión con reportes del módulo de comunidad

--- src/comunidad/dto/index.ts
+++ src/comunidad/dto/index.ts
@@ -1,3 +1,5 @@
 export * from './create-group.dto';
-export * from './send-message.dto';
 export * from './send-message-body.dto';
+export * from './send-message.dto';
+export * from './create-report.dto';
+export * from './update-report-status.dto';
