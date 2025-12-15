6e6edcee0b869a79e8f5cc1017072f6370fa1998
AlejandroHenao2572 <david.patacon-h@mail.escuelaing.edu.co>
2025-12-15 01:59:06 -0500

    fix: resolve conflicts

--- src/tutorias/tutorias.service.ts
+++ src/tutorias/tutorias.service.ts
@@ -40,7 +40,8 @@ export class TutoriasService {
       throw error;
     }
   }
-
+  
+  //
   async updateAvailabilityByEmail(email: string, disponibilidad: any, req: Request) {
     const config = JwtForwardingHelper.getAxiosConfig(req);
     const url = `${this.tutoriasManagementServiceUrl}/api/users/email/${email}/availability`;
